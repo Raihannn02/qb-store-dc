737ecf9b46a14127130c95ca685084f87b29b63c
Raihannn02 <raihanputragns@gmail.com>
2026-05-07 10:26:45 +0800

    fix: add websocket support for node 20

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,14 @@
 require('dotenv').config();
 const { createClient } = require('@supabase/supabase-js');
+const WebSocket = require('ws');
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY, {
+    auth: {
+        persistSession: false
+    },
+    realtime: {
+        websocket: WebSocket
+    }
+});
 
 module.exports = supabase;
